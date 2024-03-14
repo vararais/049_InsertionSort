a0e449b09a7086aa71b7ddeed750e7ab745d5755
vararais <varadigmaisnann@gmail.com>
2024-03-14 18:20:03 +0700

    Membuat Prosedur Input

--- InsertionShort/InsertionShort.cpp
+++ InsertionShort/InsertionShort.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 using namespace std;
 
-void insertionSort() {
-    int i, n, temp, j;
-    int arr[9];
+int arr[9];
+int n;
+
+void input() {
+	while (true) {
+		cout << "Masukkan Banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
+		}
+	}
+
+	cout << endl;
+	cout << "=====================" << endl;
+	cout << "Masukkan Elemen Array Nya" << endl;
+	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
 }
 
-int main()
-{
-    
-}
