6777a010bf6dc4454b954d045488dfea2648d908
vararais <varadigmaisnann@gmail.com>
2024-03-14 18:20:46 +0700

    Membuat InsertionSort

--- InsertionShort/InsertionShort.cpp
+++ InsertionShort/InsertionShort.cpp
@@ -26,3 +26,23 @@ void input() {
 	}
 }
 
+void insertionSort() {
+	int i, temp, j;
+
+
+	for (i = 1; i <= n - 1; i++) {
+
+		temp = arr[i];
+
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > temp)
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+
+		arr[j + 1] = temp;
+	}
+}
+
